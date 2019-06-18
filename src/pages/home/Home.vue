3a21617beed1f09dea8b0bca169bf0f840b5a6c4
huanshenyi <txy1226052@gmail.com>
2019-06-18 21:42:28 +0900

    search-bar ok

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-nav></home-nav>
+    <home-search></home-search>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeNav from './components/Nav'
+import HomeSearch from './components/Search'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeNav
+    HomeNav,
+    HomeSearch
   }
 }
 </script>
---
+++ src/pages/home/components/Search.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+      <div class="setop"></div>
+      <div class="only_search">
+        <div class="m-search">
+          <div class="config_search">
+             <span class="center">pythonナイス</span>
+               <a href="" class="ziti">
+                  <svg class="icon">
+                     <use xlink:href="#icon-fangdajing"></use>
+                  </svg>
+               </a>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSearch'
+}
+</script>
+
+<style scoped lang="stylus">
+  .setop
+    height:2.9rem
+  .only_search
+    height 2.9rem
+    background #F6F6F6
+    position relative
+    .m-search
+     position absolute
+     width 90%
+     height 2rem
+     left 50%
+     top:50%
+     transform translate(-50%,-50%)
+     .config_search
+      width 100%
+      height 100%
+      border-radius 1.5rem
+      border 1px solid #87cefa
+      background #FFF
+      .center
+       display block
+       margin-right 5rem
+       overflow hidden
+       line-height 2rem
+       text-align center
+       font-size .9rem
+       color #666
+      .ziti
+        display block
+        position absolute
+        top:.04rem
+        right -.2rem
+        width 4rem
+        height 2rem
+        background #87cefa
+        border-bottom-right-radius 1.5rem
+        border-top-right-radius 1.5rem
+        .icon
+          position relative
+          display inline-block
+          line-height 1.9rem
+          left 50%
+          top 50%
+          transform translate(-50%,-50%)
+          width: 1.9em;
+          height: 1.9em;
+          vertical-align: -0.15em;
+          fill: currentColor;
+          overflow: hidden;
+</style>
