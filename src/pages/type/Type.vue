6320ca912adade5221ceea21e555ba49f6fb166f
huanshenyi <txy1226052@gmail.com>
2019-07-02 21:58:36 +0900

    自作foot完成

--- src/pages/type/Type.vue
+++ src/pages/type/Type.vue
@@ -2,21 +2,24 @@
     <div class="type">
       <home-header></home-header>
       <class></class>
-      <home-footer></home-footer>
+<!--      <home-footer></home-footer>-->
+      <bottom-nav></bottom-nav>
     </div>
 </template>
 
 <script>
 import HomeHeader from '../home/components/Header'
 import Class from './components/Class'
-import HomeFooter from '../home/components/Footer'
+// import HomeFooter from '../home/components/Footer'
+import BottomNav from './components/Bottomnav'
 
 export default {
   name: 'Type',
   components: {
     HomeHeader,
     Class,
-    HomeFooter
+    // HomeFooter
+    BottomNav
   }
 }
 </script>
---
+++ src/pages/type/components/Bottomnav.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="dinav">
+      <div class="dinav_border a">
+         <router-link to="/">
+           <div class="inmei">
+             <div class="cont">
+               <i class="iconfont">&#xe626;</i>
+             </div>
+           </div>
+         </router-link>
+        <router-link to="/type">
+          <div class="inmei">
+            <div class="cont">
+              <i class="iconfont">&#xe626;</i>
+            </div>
+          </div>
+        </router-link>
+        <router-link to="/">
+          <div class="inmei">
+            <div class="cont">
+              <img src="../../../assets/logo.png" class="img-cen">
+            </div>
+          </div>
+        </router-link>
+        <router-link to="/">
+          <div class="inmei">
+            <div class="cont">
+              <i class="iconfont">&#xe626;</i>
+            </div>
+          </div>
+        </router-link>
+        <router-link to="/">
+          <div class="inmei">
+            <div class="cont">
+              <i class="iconfont">&#xe626;</i>
+            </div>
+          </div>
+        </router-link>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Bottomnav'
+}
+</script>
+
+<style scoped lang="stylus">
+.dinav
+  height 4.4rem
+  .dinav_border
+   position fixed
+   left 0
+   bottom 0
+   right 0
+   z-index: 1001
+  .a
+   height 4.4rem
+   background  #fff
+   .inmei
+     display block
+     width :20%
+     height 4.4rem
+     float left
+     text-align center
+     position relative
+     .cont
+      position absolute
+      top:50%
+      left 50%
+      transform translate(-50%,-50%)
+      .iconfont
+        font-size 1.5rem
+        color #999
+      .content
+        display block
+        margin-top .3rem
+      .img-cen
+        height 4rem
+</style>
